4f3cb11e192d5b2748e0a41509d8a70631269858
Chad Homan <choman@gmail.com>
2018-03-16 19:01:51 -0500

    put in a toggle for sudo

--- runme.sh
+++ runme.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-time sudo ansible-pull -o -U https://github.com/choman/ansible.git
+
+without_sudo=true
+without_sudo=false
+
+if $without_sudo; then
+    time ansible-pull -o -U https://github.com/choman/ansible.git
+else
+    time sudo ansible-pull -o -U https://github.com/choman/ansible.git
+fi
