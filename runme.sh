8eb0ba1db20953a89a7d0dacb58f8bc35f43d0b9
Chad Homan <choman@gmail.com>
2018-03-17 15:32:40 -0500

    Change with runtime option

--- runme.sh
+++ runme.sh
@@ -4,8 +4,4 @@
 without_sudo=true
 without_sudo=false
 
-if $without_sudo; then
-    time ansible-pull -o -U https://github.com/choman/ansible.git
-else
-    time sudo ansible-pull -o -U https://github.com/choman/ansible.git
-fi
+time $1 ansible-pull -o -U https://github.com/choman/ansible.git
